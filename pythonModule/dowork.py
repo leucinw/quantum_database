10b09afbcdcd945c295ed3e54ee4d8bf17bba497
Chengwen Liu <liuchw2010@gmail.com>
2019-09-06 15:14:44 -0500

    python modules

---
+++ pythonModule/dowork.py
@@ -0,0 +1,41 @@
+#!/usr/bin/env python
+
+
+import os,sys
+import numpy as np
+from readChemFile import *
+from readDataFile import *
+from operation import *
+from chemFileConvert import *
+
+#XYZ2COM_BSSE("A24-11-dimer.xyz", 6, "0,1 0,1 0,1") 
+#XYZ2COM_BSSE("A24-13-dimer.xyz", 6, "0,1 0,1 0,1")  
+#XYZ2COM_BSSE("A24-14-dimer.xyz", 6, "0,1 0,1 0,1")  
+#XYZ2COM_BSSE("A24-17-dimer.xyz", 8, "0,1 0,1 0,1")  
+XYZ2COM_BSSE("A24-18-dimer.xyz", 5, "0,1 0,1 0,1", optCartesian=True)  
+XYZ2COM_BSSE("A24-19-dimer.xyz", 5, "0,1 0,1 0,1", optCartesian=True)  
+#XYZ2COM_BSSE("A24-23-dimer.xyz", 6, "0,1 0,1 0,1")  
+#XYZ2COM_BSSE("A24-5-dimer.xyz", 4, "0,1 0,1 0,1")  
+#XYZ2COM_BSSE("A24-9-dimer.xyz", 4, "0,1 0,1 0,1")  
+
+#XYZ2COM_BSSE("S22-10-dimer.xyz", 12, "0,1 0,1 0,1")  
+#XYZ2COM_BSSE("S22-12-dimer.xyz", 10, "0,1 0,1 0,1")  
+#XYZ2COM_BSSE("S22-14-dimer.xyz", 12, "0,1 0,1 0,1")  
+#XYZ2COM_BSSE("S22-15-dimer.xyz", 15, "0,1 0,1 0,1")  
+#XYZ2COM_BSSE("S22-18-dimer.xyz", 12, "0,1 0,1 0,1")  
+#XYZ2COM_BSSE("S22-21-dimer.xyz", 12, "0,1 0,1 0,1")  
+#XYZ2COM_BSSE("S22-22-dimer.xyz", 13, "0,1 0,1 0,1")  
+#XYZ2COM_BSSE("S22-3-dimer.xyz", 5, "0,1 0,1 0,1")  
+#XYZ2COM_BSSE("S22-4-dimer.xyz", 6, "0,1 0,1 0,1")  
+#XYZ2COM_BSSE("S22-6-dimer.xyz", 12, "0,1 0,1 0,1")  
+#XYZ2COM_BSSE("S22-7-dimer.xyz", 15, "0,1 0,1 0,1")  
+#XYZ2COM_BSSE("S22-9-dimer.xyz", 6, "0,1 0,1 0,1") 
+ 
+#LOG2COM_BSSE("S22-12-dimer.out") 
+#LOG2COM_BSSE("S22-14-dimer.out") 
+#LOG2COM_BSSE("S22-15-dimer.out") 
+#LOG2COM_BSSE("S22-18-dimer.out") 
+#LOG2COM_BSSE("S22-21-dimer.out") 
+#LOG2COM_BSSE("S22-22-dimer.out") 
+#LOG2COM_BSSE("S22-6-dimer.out") 
+#LOG2COM_BSSE("S22-7-dimer.out") 
